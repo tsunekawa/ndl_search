c1f14fdf4832071e4b9805287f9bbeb47057431c
Tsunekawa Mao <tsunekaw@slis.tsukuba.ac.jp>
2013-09-07 23:08:32 +0900

    add accessors of NDLSearch::RdfItem#title, creators, publishers, subjects, classifications, et al

--- lib/ndl_search.rb
+++ lib/ndl_search.rb
@@ -83,14 +83,73 @@ module NDLSearch
       }
     end
 
-    def permalink
-      @guid ||=  doc.at('guid').text
+    def creators
+      @creators ||= doc.xpath('//dcterms:creator/foaf:Agent').inject(Array.new) do |array, creator|
+	array << {
+	  :full_name => creator.at('./foaf:name').content,
+	  :full_name_transcription => creator.at('./dcndl:transcription').try(:content),
+	  :agent_identifier => creator.attributes["about"].try(:content)
+	}
+	array
+      end
+    end
+
+    def subjects
+      @subjects ||= doc.xpath('//dcterms:subject/rdf:Description').inject(Array.new) do |array,subject|
+	array << {
+	  :term => subject.at('./rdf:value').content,
+	  #:url => subject.attribute('about').try(:content)
+        }
+	array
+      end
+    end
+
+    def classifications
+      @classifications ||= doc.xpath('//dcterms:subject[@rdf:resource]').inject(Array.new) do |array, classification|
+	array << {
+	  :url => classification.attributes["resource"].content
+	}
+        array
+      end
     end
 
     def language
       @language  ||= doc.at('//dcterms:language[@rdf:datatype="http://purl.org/dc/terms/ISO639-2"]')
                         .try(:content)
 		        .try(:downcase)
+    end
+
+    def publishers
+      @publishers ||= doc.xpath('//dcterms:publisher/foaf:Agent').inject(Array.new) do |publishers, publisher|
+	publishers << {
+	  :full_name => publisher.at('./foaf:name').content,
+	  :full_name_transcription => publisher.at('./dcndl:transcription').try(:content),
+	  :agent_identifier => publisher.attributes["about"].try(:content)
+	}
+	publishers
+      end
+    end
+
+    def extent
+      if @extent.blank? then
+        extent = doc.at('//dcterms:extent').try(:content)
+        value = {:start_page => nil, :end_page => nil, :height => nil}
+	if extent
+	  extent = extent.split(';')
+	  page = extent[0].try(:strip)
+	  if page =~ /\d+p/
+	    value[:start_page] = 1
+	    value[:end_page] = page.to_i
+	  end
+	  height = extent[1].try(:strip)
+	  if height =~ /\d+cm/
+	    value[:height] = height.to_i
+	  end
+	end
+	@extent = value
+      else
+	@extent
+      end
     end
 
     def ndc
